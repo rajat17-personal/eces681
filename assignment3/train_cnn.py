21e2309091309356c17054ee04cfb10427ba3eb8
Rajat Sharma <s.rajat1702@gmail.com>
2025-08-07 17:28:50 -0400

    final updated files

---
+++ assignment3/train_cnn.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from ray.air import session
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(64 * 8 * 8, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        return self.fc(x)
+
+def train_cnn_model(config, train_loader=None, val_loader=None, device='cuda'):
+    model = CNN().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config["reg"])
+    criterion = nn.CrossEntropyLoss()
+
+    for _ in range(config["epochs"]):
+        model.train()
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    model.eval()
+    correct, total = 0, 0
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs, 1)
+            correct += (predicted == labels).sum().item()
+            total += labels.size(0)
+
+    session.report({"val_accuracy": correct / total})
